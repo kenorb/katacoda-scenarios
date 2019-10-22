b97d6255412c32a4b114d52883ddc01b6c5a49d6
Rafal W <rafal.wieczorek@r3.com>
2019-10-22 20:04:12 +0100

    Updates steps in solution.sh

--- corda-ansible-install/solution.sh
+++ corda-ansible-install/solution.sh
@@ -4,4 +4,6 @@ git clone https://github.com/corda/corda-ansible # Step 1
 apt --yes --no-install-recommends install ansible python3-lxml # Step 2 & 6
 printf -- "---\n- hosts: localhost\n  roles:\n  - role: ./corda-ansible" > "$HOME"/corda-ansible.yml # Step 3
 sed -i'' s/corda_password_key_store/corda_password_keystore/ "$HOME"/corda-ansible/defaults/main.yml # Step 5
-ansible-playbook corda-ansible.yml # Step 4
+wget -P /root/files http://devnet-doorman.cordaconnect.io/certificates/network-root-truststore.jks # Step 7
+mkdir -v group_vars # Step 8
+ansible-playbook corda-ansible.yml && touch corda-ansible.retry # Step 4
