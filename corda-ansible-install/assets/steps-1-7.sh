902e84b33bbaf34d1b210e04db16142e8c8b6e12
Rafal Wieczorek <rafal.wieczorek@r3.com>
2020-03-10 14:00:50 +0000

    Adds extra info to correct Maven repo URL

--- corda-ansible-install/assets/steps-1-7.sh
+++ corda-ansible-install/assets/steps-1-7.sh
@@ -5,5 +5,6 @@ git clone https://github.com/corda/corda-ansible # Step 1
 apt --yes --no-install-recommends install ansible python3-lxml # Step 2 & 6
 printf -- "---\n- hosts: localhost\n  roles:\n  - role: ./corda-ansible" > "$HOME"/corda-ansible.yml # Step 3
 sed -i'' s/corda_password_key_store/corda_password_keystore/ "$HOME"/corda-ansible/defaults/main.yml # Step 5
+sed -i'' "s@http://repo1.maven.org@https://repo1.maven.org@g" "$HOME"/corda-ansible/tasks/source_maven.yml # Step 6
 wget -P /root/files http://devnet-doorman.cordaconnect.io/certificates/network-root-truststore.jks # Step 7
 ansible-playbook corda-ansible.yml && touch corda-ansible.retry # Step 4
--- corda-ansible-install/step6.md
+++ corda-ansible-install/step6.md
@@ -2,7 +2,8 @@ Now we need to solve the final dependencies.
 
 ## Task
 
-Please solve any missing dependencies (such as Python libraries) by using Apt or Pip and re-run the playbook.
+1. Please solve any missing dependencies (such as Python libraries) by using Apt or Pip and re-run the playbook.
+2. In case of _HTTP Error 501_, please correct the Maven repository URL to use HTTPS.
 
 ## Expected outcome
 
